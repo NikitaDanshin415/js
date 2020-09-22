9a87a6c5f8484ea74b0dcdbf8f006dfc2989c625
Nikita Danshin <danshinNP@yandex.ru>
2020-09-22 08:32:38 +0500

    Добавил вызов через рекурсию

--- BindCallTimer/task5.js
+++ BindCallTimer/task5.js
@@ -2,8 +2,8 @@
 //ДЗ 5*: Посмотреть на класс Date и выводить новое время в консоль при наступлении новой секунды
 
 //process.nextTick(printCurrentTime); setimmediate только в NodeJs
-let date = new Date();
-const getCurrentTime = () =>{
+
+(function printCurrentTime(){
     let date = new Date();
 
     let hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
@@ -11,10 +11,5 @@ const getCurrentTime = () =>{
     let seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
 
     console.log(`${hours}:${minutes}:${seconds}`);
-}
-
-
-setInterval(getCurrentTime, 1000)
-
-//process.nextTick(printCurrentTime); setimmediate; только в NodeJs
-// над этим еще думаю
+    setTimeout(printCurrentTime, 1000);
+})();
